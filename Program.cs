5bf082989c21539cd3f5aaf122e3b5a2db85d451
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-12 12:37:34 +0000

    feat: implemented implicit and explicit conversionts in Task01.

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@ class Program
     public static void Main()
     {
         Task00.Run();
+        Task01.MultiplyInt(((int)2.2), ((int)4.9m));
+        Console.WriteLine(Task01.MultiplyInt(((int)2.2), ((int)4.9m)));
     }
 }
--- Task01.cs
+++ Task01.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace SprintExercise_12_03_2026
 {
-    internal class Task01
+    public class Task01
     {
+        /* public static void Run()
+        {
+            float float1 = 3.80f;
+            string str1 = "h";
+            double double1 = 2.5;
+            int num1 = 2;
+            decimal dec1 = num1;
+            // the one above is called `Implicit Conversion`: int can be converted to float, double, decimal without casting
+            Console.WriteLine(dec1);
+            decimal decimal1 = 1.1m;
+            double double2 = (double)decimal1;
+            // the one above is called `Explicit Conversione`: float and double use `binary floating-point math (Base-2)`, wherase decimal uses `decimal floating-point math (Base-10)` so it needs casting
+            Console.WriteLine(double2);
+            float float2 = 2.9f;
+            int num2 = (int)float2;
+            // we need an Explicit Conversiont when converting from an higher precision to a lower precision
+            Console.WriteLine(num2);
+            decimal decimal3 = (decimal)float2;
+            Console.WriteLine(decimal3);
+            double double3 = 3.678908765436578;
+            float float3 = (float)double3;
+            Console.WriteLine(float3);
+            // we can implicitly convert a float into a double:
+            float float4 = 4.15f;
+            double double4 = float4;
+        }
+        */
+        public static int MultiplyInt(int num1, int num2)
+        {
+            return num1 * num2;
+        }
     }
 }
